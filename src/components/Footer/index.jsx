2e91272d1ee9f5a2060c674d08e23ed768a0afe3
bervalim <bernardogvalim@gmail.com>
2024-03-14 11:50:57 -0300

    feat/email

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,6 +2,7 @@ import { user } from "../../data/user";
 import whatsappIcon from "../../assets/whatsappIcon.png";
 import linkedinIcon from "../../assets/linkedinIcon.png";
 import githubIcon from "../../assets/githubIcon.png";
+import emailIcon from "../../assets/icons-email.png";
 import styles from "./style.module.css";
 import { useTranslation } from "react-i18next";
 
@@ -14,20 +15,33 @@ export const Footer = () => {
           <div className={styles.flexbox}>
             <h2 className="title md">{t("Contato")}</h2>
             <div className={styles.imageContent}>
-              <a href="https://wa.me/41984969334">
-                <img src={whatsappIcon} alt="whatsappIcon" />
-              </a>
-              <a href="https://www.linkedin.com/in/bernardo-valim/">
-                <img src={linkedinIcon} alt="linkedinIcon" />
-              </a>
-              <a href="https://github.com/bervalim">
-                <img src={githubIcon} alt="githubIcon" />
-              </a>
+              <div>
+                <a href="https://wa.me/41984969334">
+                  <img src={whatsappIcon} alt="whatsappIcon" />
+                </a>
+              </div>
+              <div>
+                <a href="https://www.linkedin.com/in/bernardo-valim/">
+                  <img src={linkedinIcon} alt="linkedinIcon" />
+                </a>
+                <span className="paragraph"></span>
+              </div>
+              <div>
+                <a href="https://github.com/bervalim">
+                  <img src={githubIcon} alt="githubIcon" />
+                </a>
+              </div>
             </div>
           </div>
-          <p className="paragraph">
-            {t("direitos")} {user}
-          </p>
+          <div>
+            {/* <p className="paragraph">
+              {t("direitos")} {user}
+            </p> */}
+            <div className={styles.email}>
+              <img src={emailIcon} />
+              <p className="paragraph">bernardogvalim@gmail.com</p>
+            </div>
+          </div>
         </div>
       </div>
     </footer>

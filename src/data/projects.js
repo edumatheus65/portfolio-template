821aa232a1d7c1136e34a3233223c673e61129f5
edumatheus65 <ematheusneves13@gmail.com>
2024-04-15 07:00:08 -0300

    fix: deleted some projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,16 +1,6 @@
 import kenzieHubImg from "../assets/Kbimage.png";
 import FashionStoreImg from "../assets/FsImage.png";
-import restaurantApiImg from "../assets/restaurantApi.jpg";
 import signContactImg from "../assets/signContactImage.png";
-import nuKenzieImg from "../assets/kenzieNu.png";
-import kenzieBurgueImg from "../assets/kenzieBurguer.png";
-import signContactAPIImg from "../assets/signContactAPI.png";
-import userCoursesAPIImg from "../assets/userCoursesAPI.png";
-import petInfoImg from "../assets/infoPet.png";
-import socialMediaImg from "../assets/redesocial.png";
-import openMusicImg from "../assets/opImage.png";
-import gitSearchImg from "../assets/gitSearchImg.png";
-import kanvasImg from "../assets/kanvasAPI.png";
 
 export const projects = [
   {
@@ -43,8 +33,8 @@ export const projects = [
     id: 3,
     name: "Kenzie Hub",
     description: "Cadastro de desenvolvedores/tecnologias",
-    applicationLink: "https://m3-s4-entrega-kenzie-hub-bervalim.vercel.app/",
-    repositoryLink: "https://github.com/bervalim/developers_hub",
+    applicationLink: "https://developers-hub-seven.vercel.app/",
+    repositoryLink: "https://github.com/edumatheus65/developers_hub",
     image: kenzieHubImg,
     techs: [
       "React Js",
@@ -54,125 +44,5 @@ export const projects = [
       "Zod",
       "React-Toastify",
     ],
-  },
-  {
-    id: 4,
-    name: "Restaurant API",
-    description: "Api para reservas em restaurantes",
-    applicationLink: "https://github.com/bervalim/RestaurantAPI",
-    repositoryLink: "https://github.com/bervalim/RestaurantAPI",
-    image: restaurantApiImg,
-    techs: ["Node Js", "TypeScript", "Express.js", "TypeORM", "PostgreSQL"],
-  },
-  {
-    id: 5,
-    name: "NU Kenzie",
-    description: "Aplicação de transações finaceiras",
-    applicationLink:
-      "https://m3-s2-entrega-portfolio-nu-kenzie-bervalim.vercel.app/",
-    repositoryLink: "https://github.com/bervalim/nukenzie",
-    image: nuKenzieImg,
-    techs: ["ReactJS", "Sass"],
-  },
-  {
-    id: 6,
-    name: "Hamburgueria",
-    description: "Aplicação de adição ao carrinho",
-    applicationLink:
-      "https://m3-s3-entrega-s3-template-hamburgueria-bervalim.vercel.app/",
-    repositoryLink: "https://github.com/bervalim/hamburgueria",
-    image: kenzieBurgueImg,
-    techs: ["ReactJS", "Sass", "Axios"],
-  },
-  {
-    id: 7,
-    name: "Sign Contact API",
-    description: "API",
-    applicationLink:
-      "https://desafio-fullstack-api.onrender.com/api-documentation/",
-    repositoryLink: "https://github.com/bervalim/sign-contact-api",
-    image: signContactAPIImg,
-    techs: [
-      "Node Js",
-      "TypeScript",
-      "Express.js",
-      "Prisma",
-      "PostgreSQL",
-      "bcrypt.js",
-    ],
-  },
-  {
-    id: 8,
-    name: "User Courses API",
-    description: "API",
-    applicationLink: "https://github.com/bervalim/userCourses-API",
-    repositoryLink: "https://github.com/bervalim/userCourses-API",
-    image: userCoursesAPIImg,
-    techs: [
-      "Node Js",
-      "TypeScript",
-      "Express.js",
-      "PG-FORMAT",
-      "Zod",
-      "Json-Web-Token",
-    ],
-  },
-  {
-    id: 9,
-    name: "PET INFO",
-    description: "aplicação",
-    applicationLink:
-      "https://kenzie-academy-brasil-developers.github.io/petinfobase_bervalim/",
-    repositoryLink: "https://github.com/bervalim/petInfo",
-    image: petInfoImg,
-    techs: ["HTML", "CSS", "JavaScript", "Fetch API"],
-  },
-  {
-    id: 10,
-    name: "Rede Social",
-    description: "aplicação",
-    applicationLink:
-      "https://kenzie-academy-brasil-developers.github.io/entrega-criando-rede-social_bervalim/",
-    repositoryLink:
-      "https://github.com/Kenzie-Academy-Brasil-Developers/entrega-criando-rede-social_bervalim",
-    image: socialMediaImg,
-    techs: ["HTML", "CSS", "JavaScript"],
-  },
-  {
-    id: 11,
-    name: "Open Music",
-    description: "aplicação",
-    applicationLink:
-      "https://kenzie-academy-brasil-developers.github.io/open-music-base_bervalim/",
-    repositoryLink: "https://github.com/bervalim/open-music",
-    image: openMusicImg,
-    techs: ["HTML", "CSS", "JavaScript"],
-  },
-  {
-    id: 12,
-    name: "Git Search",
-    description: "aplicação",
-    applicationLink:
-      "https://kenzie-academy-brasil-developers.github.io/gitSearchBase_bervalim/",
-    repositoryLink: "https://github.com/bervalim/gitSearch",
-    image: gitSearchImg,
-    techs: ["HTML", "CSS", "JavaScript", "Fetch API"],
-  },
-  {
-    id: 13,
-    name: "kanvas",
-    description: "aplicação",
-    applicationLink: "https://github.com/bervalim/kanvas-api",
-    repositoryLink: "https://github.com/bervalim/kanvas-api",
-    image: kanvasImg,
-    techs: [
-      "Python",
-      "Django",
-      "DRF",
-      "Postgres",
-      "Swagger",
-      "Concrete Views",
-      "Model Serializer",
-    ],
-  },
+  },   
 ];
